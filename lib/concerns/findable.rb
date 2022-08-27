5121f65ec1e0e8d0010426cdf7c24a547a354b60
wachira <wachiraa954@gmail.com>
2022-08-28 01:34:06 +0300

    done

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,9 @@
+module Findable
+    def find_by_name(name)
+        self.class.all.detect{|a| a.name == name}
+    end
+  
+    def find_by_name(name)
+        self.class.all.detect{|a| a.name == name}
+    end
+  end
